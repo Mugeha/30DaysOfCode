c76be156d0be47a4408bdf2fa2073712f34b08cd
Mugeha <mugehajacky@gmail.com>
2025-06-16 19:37:11 +0300

    Updated solution

---
+++ 30DaysOfCode/Day1.js
@@ -0,0 +1,17 @@
+
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    // Declare second integer, double, and String variables.
+// Use 'readLine' to get inputs from stdin
+    var i2 = parseInt(readLine());
+    var d2 = parseFloat(readLine());
+    var s2 = readLine();
+    // Read and save an integer, double, and String to your variables.
+
+    // Print the sum of both integer variables on a new line.
+ console.log(i + i2);
+    console.log((d + d2).toFixed(1));
+    console.log(s + s2);
+}
