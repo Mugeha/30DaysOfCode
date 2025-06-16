0ef2966f297403475f1b355665b4dca4b20c994b
Mugeha <mugehajacky@gmail.com>
2025-06-16 19:37:39 +0300

    Updated solution

--- 30DaysOfCode/Day0.js
+++ 30DaysOfCode/Day0.js
@@ -6,15 +6,3 @@ function processData(inputString) {
     
     console.log(inputString);
 }
-
-
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
-});
-
-process.stdin.on("end", function () {
-   processData(_input);
-});
